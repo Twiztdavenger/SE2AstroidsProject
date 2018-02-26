d160ab2fb26467fd4852ad470b639281727813bc
Matthew Muenzberg <mmuenzbe15@mail.bw.edu>
2018-02-26 17:17:46 -0500

    Setting up trialController

--- Assets/Scripts/Trial.cs
+++ Assets/Scripts/Trial.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Trial : MonoBehaviour {
 
+    
+
+    // Ship attributes
+    public Vector3 shipSpawn = new Vector3(0, 0, 0);
+
     public bool shipMove = false;
     public bool shipRotate = false;
 
-    public Vector3 shipSpawn = new Vector3(0, 0, 0);
+    public float shipMoveSpeed = 1.5f;
+    public float shipRotateSpeed = 180f;
 
-    // We can implement the circle spawn idea and 
-    //      do the math in AsteroidController
+    
+
+    // Asteroid Object
+    public GameObject asteroidPrefab;
+
+    // Asteroid attributes
     public float AsteroidMovementX = 0;
     public float AsteroidMovementY = 0;
+    public float AsteroidRotation = 180f;
+
+    public Vector3 AsteroidSpawn = new Vector3(0, 0, 0);
     
 
 	// Use this for initialization
@@ -22,6 +35,15 @@ public class Trial : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        //GameObject tempAstroid = AsteroidList.Dequeue();
+
+        GameObject tempAstroid = asteroidPrefab;
+
+        tempAstroid.GetComponent<Asteroid>().rotationSpeed = AsteroidRotation;
+        tempAstroid.GetComponent<Asteroid>().movementSpeedX = AsteroidMovementX;
+        tempAstroid.GetComponent<Asteroid>().movementSpeedY = AsteroidMovementY;
+        tempAstroid.GetComponent<Asteroid>().rotation = true;
+
+        Instantiate(tempAstroid, AsteroidSpawn, transform.rotation);
+    }
 }
